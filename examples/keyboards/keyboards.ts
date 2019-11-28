cb5154457fa53bc8e6ce1e15ecf40a5582ca5c58
matt <matt.bark11@gmail.com>
2019-11-28 14:02:25 -0500

    Add first examples

---
+++ examples/keyboards/keyboards.ts
@@ -0,0 +1,28 @@
+import QMK, { Keyboard } from '../../src';
+
+export const promiseKeyboardExample = (): Promise<Keyboard> => {
+  // Create an API client using the provided version
+  const client = new QMK('v1');
+  const keyboardName = 'massdrop/alt';
+
+  // Fetch keyboard metadata
+  return client.keyboards(keyboardName).then(({ keyboards }) => {
+    // The "keyboards" call always returns a dictionary of keyboards
+    // So we keep keyboardName to reference it in the response body
+    const keyboard: Keyboard = keyboards[keyboardName];
+    console.info(`${keyboard.keyboard_name} has ${Object.keys(keyboard.layouts).length} layouts`);
+    return keyboard;
+  });
+};
+
+export const asyncKeyboardExample = async (): Promise<Keyboard> => {
+  // Create an API client using the provided version
+  const client = new QMK('v1');
+  const keyboardName = 'massdrop/alt';
+
+  // Fetch keyboard metadata
+  const { keyboards } = await client.keyboards(keyboardName);
+  const keyboard: Keyboard = keyboards[keyboardName];
+  console.info(`${keyboard.keyboard_name} has ${Object.keys(keyboard.layouts).length} layouts`);
+  return keyboard;
+};
