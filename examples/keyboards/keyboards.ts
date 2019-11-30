8a95590aa8f2020d38f58e2d6ff36a98bfab912d
matt <matt.bark11@gmail.com>
2019-11-29 19:26:04 -0500

    rename keyboard examples

--- examples/keyboards/keyboards.ts
+++ examples/keyboards/keyboards.ts
@@ -1,21 +1,20 @@
 import QMK, { Keyboard } from '../../src';
 
-export const promiseKeyboardExample = (): Promise<Keyboard> => {
+export const keyboardExample = async (): Promise<Keyboard> => {
   // Create an API client using the provided version
   const client = new QMK('v1');
   const keyboardName = 'massdrop/alt';
 
   // Fetch keyboard metadata
-  return client.keyboards(keyboardName).then(({ keyboards }) => {
-    // The "keyboards" call always returns a dictionary of keyboards
-    // So we keep keyboardName to reference it in the response body
-    const keyboard: Keyboard = keyboards[keyboardName];
-    console.info(`${keyboard.keyboard_name} has ${Object.keys(keyboard.layouts).length} layouts`);
-    return keyboard;
-  });
+  const { keyboards } = await client.keyboards(keyboardName);
+  // The "keyboards" call always returns a dictionary of keyboards
+  // So we keep keyboardName to reference it in the response body
+  const keyboard: Keyboard = keyboards[keyboardName];
+  console.info(`${keyboard.keyboard_name} has ${Object.keys(keyboard.layouts).length} layouts`);
+  return keyboard;
 };
 
-export const asyncKeyboardsExample = async (): Promise<Keyboard[]> => {
+export const keyboardsExample = async (): Promise<Keyboard[]> => {
   // Create an API client using the provided version
   const client = new QMK('v1');
 
